abdfce1d1ffa39a56cbd01898bcd8ed35497a855
Narak1996 <41143997+Narak1996@users.noreply.github.com>
2018-08-06 10:33:31 +0700

    In this invoice, which record who delivery first is on top

--- erp/models/Sales_model.php
+++ erp/models/Sales_model.php
@@ -5530,7 +5530,7 @@ class Sales_model extends CI_Model
                             GROUP BY
                                 `erp_deliveries`.`location` 
                                 
-                            ORDER BY  location DESC"
+                            ORDER BY  date1 ASC"
          );
 
         return $query->result();
